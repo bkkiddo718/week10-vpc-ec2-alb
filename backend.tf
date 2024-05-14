fa140457a57275e9a562e406d3e8641df9118a84
Kwame <kwame.stewart@yahoo.com>
2024-05-14 10:41:21 -0400

    new

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "week10-ks-terraform"
+    bucket         = "week10-kms-terraform"
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
